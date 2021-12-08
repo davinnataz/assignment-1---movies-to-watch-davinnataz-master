21bd0b15c406c8c0b077d4396ef110a003f85393
Davin Natanael <davinnatanael@my.jcu.edu.au>
2021-12-08 21:04:44 +0700

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+"""
+Replace the contents of this module docstring with your own details
+Name: Davin Natanael
+Date started: 08 December 2021
+GitHub URL: 
+"""
+
+
+def main():
+    """..."""
+    print("Movies To Watch 1.0 - by Davin Natanael")
+
+
+if __name__ == '__main__':
+    main()
