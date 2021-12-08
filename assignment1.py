f6f86149290aa713efa1fda950100916e62a187a
Davin Natanael <davinnatanael@my.jcu.edu.au>
2021-12-09 00:01:16 +0700

    adding a watch movies function and adding a comment in there

--- assignment1.py
+++ assignment1.py
@@ -9,11 +9,11 @@ import csv
 import operator
 
 def read_file():
-    #this function is to transfer data from movies.csv
+    #this function to open a movies.csv files for adding a movie there
     csv_file = open('movies.csv')
-    csv_read = csv.reader(csv_file, delimiter =',')
+    csv_read = csv.reader(csv_file, delimiter=',')
     total_data = 0
-    movie_list =[]
+    movies_list = []
     for row in csv_read:
         movie = []
         movie.append(row[0])
@@ -21,61 +21,98 @@ def read_file():
         movie.append(row[2])
         movie.append(row[3])
         total_data += 1
-        movie_list.append(movie)
-        return total_data, movie_list
-
+        movies_list.append(movie)
+    return total_data, movies_list
 
 def main():
-    # this is a main menu for movie to watch
+    #this is a main function from this program
     """..."""
     total_data, movies_list = read_file()
+
     print("Movies To Watch 1.0 - by Davin Natanael")
     print(f"{total_data} movies loaded")
+
     while True:
         print_menu()
-        menu = input(">>>").upper
-        if menu == 'L':
-            total_data, movie_list = movies_list(total_data, movie_list)
-        elif menu == 'A':
-            total_data, movie_list = adding_movie(total_data, movie_list)
-        elif menu == 'W':
-            total_data, movie_list = watching_movie(total_data, movie_list)
-        elif menu == 'Q':
-            quit_menu(total_data, movie_list)
+        menu = input(">>> ").upper()
+        if (menu == 'L'):
+            total_data, movies_list = Listed_movies(total_data,movies_list)
+        elif (menu == 'A'):
+            total_data, movies_list = add_movie(total_data,movies_list)
+        elif (menu == 'W'):
+            total_data, movies_list = watch_movies(total_data,movies_list)
+        elif (menu == "Q"):
+            quit_movies(total_data, movies_list)
             break
-        else:
-            print("Menu is not found")
+        else:   
+            print("Menu not found")
 
 
-
-
-
-def print_menu():
-    #to show a menu
-    print("Menu : ")
-    print("L - List movie")
+def print_menu():  
+    print("Menu:")
+    print("L - List movies")
     print("A - Add new movie")
     print("W - Watch a movie")
     print("Q - Quit")
 
-
-def movies_list(total_data, movie_list):
-    #to show a list movie and knowledge if it is watched or not
+def Listed_movies (total_data,movies_list):
+    #this function is to show the list of movie and give knowledge if this movie is watched or not
     i = 0
     u = 0
     w = 0
-    movie_list.sort(key=operator.itemgetter(1))
-    for row in movie_list:
-        i += 1
-        if row[3] == "u":
+    movies_list.sort(key=operator.itemgetter(1))
+    for row in movies_list:
+        if (row[3] == "u"):
+            print(f"{i}. * {row[0]} \t\t\t - {row[1]} ({row[2]})")
             u += 1
-            print(f"{i}. * {row[0]} - {row[1]} ({row[2]})")
         else:
+            print(f"{i}.   {row[0]} \t\t\t - {row[1]} ({row[2]})")
             w += 1
-            print(f"{i}. * {row[0]} - {row[1]} ({row[2]})")
+        i += 1
     print(f"{w} movie watched, {u} movies still to watch")
-    return total_data, movie_list
+    return total_data, movies_list
+
+
+def watch_movies(total_data, movies_list):
+    #this function is to watch a movie
+    j = 0
+    for row in movies_list:
+        if (row[3] == 'w'):
+            j += 1
+
+    if (j == total_data):
+        print("no more movies to watch")
+        return total_data, movies_list
+
+    print("Enter the number of a movie to mark as watched")
+    while True:
+
+        try:
+            menu = int(input(">>> "))
+        except ValueError:
+            print("Invalid Input; Enter a valid number")
+            continue
+
+        if (total_data == 0):
+            print("no records found")
+        elif (menu < 0):
+            print("Number must be >= 0")
+        elif (menu >= total_data):
+            print("Invalid movie number")
+        else:
+            if (movies_list[menu][3] == "w"):
+                print(f"You have already watched {movies_list[menu][0]}")
+            else:
+                movies_list[menu][3] = "w"
+                print(f"{movies_list[menu][0]} from {movies_list[menu][1]} watched")
+            break
+
+    return total_data, movies_list
+
+
+
 
 
 if __name__ == '__main__':
     main()
+
