4ad96228a1543e68b271c5fdef647f0a28e75f74
Davin Natanael <davinnatanael@my.jcu.edu.au>
2021-12-08 23:20:49 +0700

    editing a main function

--- assignment1.py
+++ assignment1.py
@@ -5,10 +5,27 @@ Date started: 08 December 2021
 GitHub URL:https://github.com/davinnataz/assignment-1---movies-to-watch-davinnataz-master.git
 """
 
+import csv
+import operator
 
 def main():
+    # this is a main menu for movie to watch
     """..."""
     print("Movies To Watch 1.0 - by Davin Natanael")
+    print(f"{total_data} movies loaded")
+    while True:
+        print_menu():
+        menu = imput(">>>").upper
+        if menu == 'L':
+
+        elif menu == 'A':
+
+        elif menu == 'W':
+
+        elif menu == 'Q':
+
+        else:
+            print("Menu is not found")
 
 
 if __name__ == '__main__':
